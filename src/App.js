c655438028afde4fa5b379e4890c151ca06ee3bc
Anees Malik <theaneesmalik@gmail.com>
2022-11-27 18:30:37 +0500

    added env

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import "./App.css";
 
 import SearchIcon from "./search.svg";
 import MovieCard from "./MovieCard";
-const API_URL = "http://www.omdbapi.com/?apikey=4ee775e";
+const API_URL =
+  "http://www.omdbapi.com/?apikey=" + process.env.REACT_APP_API_KEY;
 
 const App = () => {
   const [movies, setMovies] = useState([]);
